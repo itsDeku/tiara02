7d18a078cc141633d33ed93f95c967294af5f6a0
saini <deepaksaini63871@gmail.com>
2020-09-04 12:34:43 -0700

    is it working

--- src/components/main.js
+++ src/components/main.js
@@ -7,7 +7,7 @@ class Main extends React.Component {
     render() {
         return(
             <main>
-                <Home />
+                <Messages />
             </main>
         )
     }
