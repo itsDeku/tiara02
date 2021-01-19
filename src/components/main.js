34b7eda265801e0ed5ed5a81979de155840c9879
itsDeku <70664666+itsDeku@users.noreply.github.com>
2021-01-19 11:52:21 +0530

    Update main.js

--- src/components/main.js
+++ src/components/main.js
@@ -3,11 +3,12 @@ import React from 'react'
 import Profile from './profile'
 import Home from  './home'
 import Messages from './messages'
+import Login from './Login'
 class Main extends React.Component {
     render() {
         return(
             <main>
-                <Messages />
+                <Login />
             </main>
         )
     }
